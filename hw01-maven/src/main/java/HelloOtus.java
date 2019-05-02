a8c895f7fcf200d1e34248092bea2d8c625cb42b
a.off <arikov@mail.ru>
2019-05-02 21:26:47 +0300

    v.1.01

---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,12 @@
+import com.google.common.math.DoubleMath;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+
+        double f = DoubleMath.factorial(5);
+        System.out.println(f);
+
+    }
+
+}
