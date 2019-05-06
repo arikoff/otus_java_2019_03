3a47a69552d1296f9e97d5ae8f5d4131967b82ef
a.off <arikov@mail.ru>
2019-05-06 13:58:03 +0300

    hw03 v.1.01

---
+++ hw03-Annotations/src/main/java/ru/otus/homework/Main.java
@@ -0,0 +1,12 @@
+package ru.otus.homework;
+
+import lombok.SneakyThrows;
+
+public class Main {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        AnnotationsTestLauncher.Launch("ru.otus.homework.AnnotationsTest");
+    }
+}
+
